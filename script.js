9bf3a270d65de449978550631e9297f3ef770e01
Ahmed ELsaman <129955390+a7m3delsman@users.noreply.github.com>
2023-12-06 21:22:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -151,3 +151,26 @@ function playClickSound() {
     clickSound.currentTime = 0;
     clickSound.play();
   }
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.getElementById('user-icon');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0%';
+}
+function close(){
+    mainMenu.style.top = '-150%';
+    return;
+}
